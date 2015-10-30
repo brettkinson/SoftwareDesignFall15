d9477e47199402c522bae3f966f736f34266ec43
brettkinson <bgatkinson@comcast.net>
2015-10-30 19:03:32 -0400

    Commit word frequency toolbox final

--- toolbox/word_frequency_analysis/frequency.py
+++ toolbox/word_frequency_analysis/frequency.py
@@ -7,7 +7,8 @@
 
 
 import string
-import collections
+from collections import Counter
+import operator
 
 def get_word_list(file_name):
 	""" Reads the specified project Gutenberg book.  Header comments,
@@ -23,6 +24,10 @@ def get_word_list(file_name):
 
 	f.close()
 
+	lines_low = []
+	lines_low_punc = []
+	words_list = []
+
 
 	while lines[curr_line].find('START OF THIS PROJECT GUTENBERG EBOOK') == -1:
 		curr_line += 1
@@ -36,28 +41,16 @@ def get_word_list(file_name):
 	lines_low = [item.lower() for item in lines]
 	lines_low = [x.replace("\r\n","") for x in lines_low]
 
-	i=0
-	for i in range(0,len(lines_low)):
-		lines_low[i] = lines_low[i].split()
-	i=0
-	j=0
-	punc = string.punctuation
-	for j in range(0,len(lines_low)):
-		for i in range(0,len(punc)):
-			lines_low[j].replace(punc[i],'')
+	for item in lines_low:
+		words_list.extend(item.split())
 
-	#i=0
-	#j=0
-	#whitespc = string.whitespace
-	#for j in range(0,len(lines_low)):
-	#	for i in range(0,len(whitespc)):
-	#		lines_low[j].replace(whitespc[i],'')
+	chars = ['.',',','-','_','?','!','\'','\"','--']
+	for item in chars:
+		words_sem_punc = [x.replace(item,'') for x in words_list]
+
+	return words_sem_punc
+
 	
-
-	print lines_low
-
-
-
 
 def get_top_n_words(word_list, n):
 	""" Takes a list of words as input and returns a list of the n most frequently
@@ -69,10 +62,21 @@ def get_top_n_words(word_list, n):
 		returns: a list of n most frequently occurring words ordered from most
 				 frequently to least frequently occurring
 	"""
-	pass
+
+	x = word_list
+	freq = {}
+	for item in word_list:
+		if item not in freq:
+			freq[item] = 0 
+		freq[item] += 1
+
+	freq_sorted = sorted(freq.iteritems(), key=operator.itemgetter(1), reverse=True)
+
+   	print freq_sorted[0:n]
+
 
 if __name__ == '__main__':
-	n = 10 # number of words to search for
+	n = 100 # number of words to search for
 	file_name = 'great_expectations_full_text.txt' # file name of book to use
-	#get_top_n_words(get_word_list(file_name),n)
-	get_word_list(file_name)
+	print get_top_n_words(get_word_list(file_name),n)
+	#get_word_list(file_name)
